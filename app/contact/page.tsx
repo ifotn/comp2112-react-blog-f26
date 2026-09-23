1f9f28d8399de101b738f82803ac995c3a11ba6e
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-09-23 10:29:57 -0400

    l3/4

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,21 @@
+'use client';
+
+import React, { useState } from "react";
+
 export default function Contact() {
+    const [name, setName] = useState<string>('');
+
+    // change event handler for textbox
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>)=> {
+        // update state var with current value of textbox
+        setName(event.target.value);
+    }
+
     return (
         <main>
             <h1>Contact Us</h1>
+            <input type="text" placeholder="Your Name" value={name} onChange={handleChange} />
+            <p>You typed: {name}</p>
         </main>
     );
 }
